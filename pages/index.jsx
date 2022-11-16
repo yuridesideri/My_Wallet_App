6d8dc58c7b17fa6fa94904c1602d4130ce17ef4e
Yuri Desideri <yuridesideri12@gmail.com>
2022-11-16 16:39:22 -0300

    feat: webapp pages setup, libraries, log-on page & styles, log-in component

---
+++ pages/index.jsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link';
+
+export default function LogIn() {
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
+  return (
+    <div className="flex container items-center flex-col justify-center min-h-screen min-w-full" >
+      <Head>
+        <title>My Wallet</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <h1 className='text-4xl font-title'>My Wallet</h1>
+      <form onSubmit={ handleSubmit } action="" className=" flex flex-col" >
+        <input type="text" placeholder='E-mail'/>
+        <input type="text" placeholder='Senha'/>
+        <button type='submit'>Entrar</button>
+      </form>
+        <Link href='/log-on'>Primeira vez? Cadastre-se</Link>
+    </div>
+  )
+}
