fd5a3b6d1ac78890f2f2bd637a7decbea6f5633c
Yuri Desideri <yuridesideri12@gmail.com>
2022-11-17 16:20:05 -0300

    feat: implemented Login & LogOn user pages

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,103 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link';
+import { Puff } from "react-loader-spinner";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
 export default function LogIn() {
-  function handleSubmit(e){
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [token, setToken] = useState(null);
+  const router = useRouter();
+  const apiUrl = process.env.NEXT_PUBLIC_API_BASE_ROUTE;
+
+  useEffect(() => {
+    if (!token) {
+      const hasTok = sessionStorage.getItem('token');
+      if (hasTok) {
+        //Checar validade do token
+      };
+    }
+  },[])
+
+  useEffect(() => {
+    if (token){
+      sessionStorage.setItem('token', token);
+      router.push("/authenticated/dashboard")
+    }
+  }, [token])
+
+  function handleUserSignIn(e){
     e.preventDefault();
+    setLoading(true);
+    const [
+      {value: email},
+      {value: password}
+    ] = e.target;
+    axios.post(apiUrl + '/sign-in', {email, password})
+    .then( ({status, data}) => {
+      if (status === 202) setToken(data);
+      setLoading(false);
+    })
+    .catch(({request}) => {
+      const status = request?.status;
+      if (status === 0) setError("Couldn't connect to server");
+      if (status === 406) setError("Wrong email and/or password");
+      setLoading(false);
+    })
   }
 
   return (
-    <div className="flex container items-center flex-col justify-center min-h-screen min-w-full" >
-      <Head>
-        <title>My Wallet</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <h1 className='text-4xl font-title'>My Wallet</h1>
-      <form onSubmit={ handleSubmit } action="" className=" flex flex-col" >
-        <input type="text" placeholder='E-mail'/>
-        <input type="text" placeholder='Senha'/>
-        <button type='submit'>Entrar</button>
-      </form>
-        <Link href='/sign-up'>Primeira vez? Cadastre-se</Link>
+  <>
+    <Head>
+
+    </Head>
+    <div className="flex h-screen flex-col items-center justify-center bg-purple-600">
+        <div className="relative flex h-[318px] w-[325px] flex-col items-center bg-purple-600">
+            <h1 className="font-title text-3xl leading-[50px] text-white">My Wallet</h1>
+            <form
+                className="my-7 flex w-full grow basis-4/6 flex-col"
+                onSubmit={handleUserSignIn}
+            >
+                {[
+                    { type: "email", placeholder: "E-mail", name: "email" },
+                    { type: "password", placeholder: "Senha", name: "password" },
+                ].map(({ type, placeholder, name }, formInd) => (
+                    <input
+                        key={`${placeholder}${formInd}`}
+                        type={type}
+                        placeholder={placeholder}
+                        name={name}
+                        className="my-1.5 w-full grow rounded-md indent-3 shadow-purple-800 placeholder:text-black focus:shadow-md focus:outline-0"
+                    />
+                ))}
+                <button
+                    className="my-1.5 w-full grow-0 h-10 rounded-md bg-[#A328D6] text-white shadow-md shadow-purple-800 flex justify-center items-center" 
+                    type="submit"
+                >
+                    {loading ? (
+                        <Puff
+                            height="30"
+                            width="30"
+                            radisu={1}
+                            color="#ffffff"
+                            ariaLabel="puff-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                            visible={true}
+                        />
+                    ) : (
+                        "Entrar"
+                    )}
+                </button>
+            </form>
+            <Link className="text-white h-10" href="/sign-up">
+              Primeira vez? Cadastre-se!
+            </Link>
+            {error && <p className="absolute bottom-[-20px] text-orange-400">{error}</p>}
+        </div>
     </div>
-  )
+  </>
+);
 }
--- pages/sign-up.jsx
+++ pages/sign-up.jsx
@@ -1,38 +1,97 @@
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { Puff } from "react-loader-spinner";
 
-export default function LogOn(props) {
-    function handleSubmit(e) {
+export default function SignUp(props) {
+    const apiUrl = process.env.NEXT_PUBLIC_API_BASE_ROUTE;
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const router = useRouter()
+
+
+    function handleUserSignUp(e) {
         e.preventDefault();
+        setLoading(true);
+        const [
+            { value: name },
+            { value: email },
+            { value: password },
+            { value: confirmPassword },
+        ] = e.target;
+        if (password !== confirmPassword) {
+            setError("Passwords do not match!");
+            setLoading(false);
+            return;
+        }
+        axios.post(apiUrl+'/sign-up', { name, email, password })
+            .then((res) => {
+                const {status} = res;
+                if (status === 201) {
+                    console.log(res.data);
+                    router.push('/');
+                }
+                setLoading(false);
+            })
+            .catch((err) => {
+                const status = err.request?.status;
+                if (status === 409) setError("This email already exists! Try loggin in!");
+                if (status === 0) setError("Couldn't Connect to Server");
+                setLoading(false);
+            });
     }
 
     return (
         <div className="flex h-screen flex-col items-center justify-center bg-purple-600">
-            <div className="flex h-[458px] w-[325px] flex-col items-center bg-purple-600">
+            <div className="relative flex h-[458px] w-[325px] flex-col items-center bg-purple-600">
                 <h1 className="font-title text-3xl leading-[50px] text-white">My Wallet</h1>
-                <form className="my-7 flex w-full grow basis-4/6 flex-col" onSubmit={handleSubmit}>
+                <form
+                    className="my-7 flex w-full grow basis-4/6 flex-col"
+                    onSubmit={handleUserSignUp}
+                >
                     {[
-                        { type: "text", placeholder: "Nome" },
-                        { type: "email", placeholder: "E-mail" },
-                        { type: "password", placeholder: "Senha" },
-                        { type: "password", placeholder: "Confirme a senha" },
-                    ].map(({type, placeholder},formInd) => (
+                        { type: "text", placeholder: "Nome", name: "name" },
+                        { type: "email", placeholder: "E-mail", name: "email" },
+                        { type: "password", placeholder: "Senha", name: "password" },
+                        {
+                            type: "password",
+                            placeholder: "Confirme a senha",
+                            name: "confirm-password",
+                        },
+                    ].map(({ type, placeholder, name }, formInd) => (
                         <input
                             key={`${placeholder}${formInd}`}
                             type={type}
                             placeholder={placeholder}
+                            name={name}
                             className="my-1.5 w-full grow rounded-md indent-3 shadow-purple-800 placeholder:text-black focus:shadow-md focus:outline-0"
                         />
                     ))}
                     <button
-                        className="my-1.5 w-full grow rounded-md bg-[#A328D6] text-white shadow-md shadow-purple-800"
+                        className="my-1.5 w-full grow-0 h-12 rounded-md bg-[#A328D6] text-white shadow-md shadow-purple-800 flex justify-center items-center" 
                         type="submit"
                     >
-                        Cadastrar
+                        {loading ? (
+                            <Puff
+                                height="30"
+                                width="30"
+                                radisu={1}
+                                color="#ffffff"
+                                ariaLabel="puff-loading"
+                                wrapperStyle={{}}
+                                wrapperClass=""
+                                visible={true}
+                            />
+                        ) : (
+                            "Cadastrar"
+                        )}
                     </button>
                 </form>
-                <Link className="text-white" href="/">
+                <Link className="text-white h-10" href="/">
                     Já tem uma conta? Entre agora!
                 </Link>
+                {error && <p className="absolute bottom-[-20px] text-orange-400">{error}</p>}
             </div>
         </div>
     );
